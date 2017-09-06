1c854c069d23ad78f753120d7430cfb1cfe58158
paragsal <parag.s@live.in>
2017-09-06 14:44:27 -0400

    Removed connectivity issues

--- sniffer.py
+++ sniffer.py
@@ -3,13 +3,67 @@ import struct
 # import textwrap
 
 def main():
-    conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-    conn.listen()
+    # #conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+    # conn = socket.socket()
+    # host = socket.gethostname()  # Get local machine name
+    # port = 4573
+    # conn.bind((host, port))
+    # conn.recvfrom(65536)
+    # print('start')
     # while True:
+    #     c, addr = conn.accept()  # Establish connection with client.
+    #     print(addr)
+    #     conn.close()
+    # # while True:
+    # #     raw_data, addr = conn.recvfrom(65536)
+    # #     dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+    # #     print("\nEthernet frame: ")
+    # #     print('Destination {}, Source {}, Protocol {]'.format(dest_mac, src_mac, eth_proto))
+
+    # # Get host
+    # host = socket.gethostbyname(socket.gethostname())
+    # print('IP: {}'.format(host))
+    #
+    # # Create a raw socket and bind it
+    # conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+    # conn.bind((host, 0))
+    #
+    # # Include IP headers
+    # conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    # # Enable promiscuous mode
+    # conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    #
+    # while True:
+    #     # Receive data
     #     raw_data, addr = conn.recvfrom(65536)
+    #
+    #     # Unpack data
     #     dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
-    #     print("\nEthernet frame: ")
-    #     print('Destination {}, Source {}, Protocol {]'.format(dest_mac, src_mac, eth_proto))
+    #
+    #     print('\nEthernet Frame:')
+    #     print("Destination MAC: {}".format(dest_mac))
+    #     print("Source MAC: {}".format(src_mac))
+    #     print("Protocol: {}".format(eth_proto))
+
+
+    HOST = socket.gethostbyname(socket.gethostname())
+    print('IP: {}'.format(HOST))
+
+
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+
+    conn.bind(('', 80))
+    #conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    #conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    print('xxxxxxxxxx')
+    print(conn.getsockname()[1])
+    conn.connect((HOST, 80))
+    while True:
+        print('yesy')
+        raw_data, addr = conn.recvfrom(65535)
+        dest_mac, src_mac, eth_proto, data = ethernet_frame(raw_data)
+        print('\nEthernet Frame:')
+        print('Destination: {}, Source: {}, Protocol: {}'.format(dest_mac, src_mac, eth_proto))
 
 
 # unpack ethernet frame
